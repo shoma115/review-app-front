5fa0a9296dac1dac453a144d7592f6a364345415
shoma115 <xiangzhendaogen8@gmail.com>
2024-01-21 19:38:54 +0900

    ログインページの作成

--- src/Certification/Login.js
+++ src/Certification/Login.js
@@ -1,34 +1,41 @@
-import { FormControl, FormLabel, Input, Button } from '@mui/material';
+import { 
+  FormControl, 
+  FormLabel, 
+  Input, 
+  InputLabel, 
+  Button 
+} from '@mui/material';
+import { useState } from 'react';
+import { useLogin } from '../Querys/AuthQuery';
 
-function Title() {
-  return (
-    <h1>ログイン</h1>
-  );
-}
+const LoginPage = () => {
+  const login = useLogin();
+  const [ email, setEmail ] = useState("test@example.com");
+  const [ password, setPassword ] = useState("shoma1030");
+  const handleLogin = (event) => {
+    // ブラウザのデフォルトの動作（リクエストを作成し、リロードを行う）をキャンセルし、設定したログイン動作を行う。
+    event.preventDefault();
+    login.mutate({email: email, password: password});
+    console.log(email)
+    console.log(password)
+  }
 
-function InputUserInfo({promptValue}) {
   return (
-    <>
-      <FormLabel>{promptValue}</FormLabel>
-      <Input></Input>
+    <>    
+      <form onSubmit={handleLogin}>
+        <FormLabel>ログイン</FormLabel>
+        <FormControl>  
+          <InputLabel>メール</InputLabel>
+          <Input></Input>
+        </FormControl>
+        <FormControl> 
+          <InputLabel>パスワード</InputLabel>
+          <Input></Input>
+        </FormControl>
+        <Button variant="contained" type="submit" >ログイン</Button>
+      </form>
     </>
-  );
-}
+  )
+} 
 
-function SubmitButton() {
-  return (
-    <Button variant="contained">ログイン</Button>
-  );
-}
-
-function Login() {
-  return (
-    <>
-      <Title />
-      <InputUserInfo />
-      <SubmitButton />
-    </>
-  );
-}
-
-export default Login;
+export default LoginPage;
