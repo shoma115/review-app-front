f99001ecf2b7e71deabe4acf77afc8284e35cb4f
shoma115 <xiangzhendaogen8@gmail.com>
2024-01-21 19:37:47 +0900

    トーストとqueryCliantをトップレベルに追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import ReviewList from "./ReviewListPage/ReviewListTable";
 import ReviewDetail from "./ReviewDetail/reviewDetailTable";
 import TopPage from "./Top/topPage";
+import LoginPage from "./Certification/Login";
+import RegisterPage from "./Certification/Register";
 import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const queryClient = new QueryClient()
 
 function App() {
+
   return (
     <>
-      <div>
-        <Routes>
-          <Route path='/' element={ <TopPage /> }></Route>
-          <Route path='/lesson' element={ <ReviewList />}></Route>
-          <Route path='/review' element={ <ReviewDetail />}></Route>
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div>
+        <ToastContainer />
+          <Routes>
+            <Route path='/' element={ <TopPage /> }></Route>
+            <Route path='/register' element={ <RegisterPage /> }></Route>
+            <Route path="/login" element={ <LoginPage /> }></Route>
+            <Route path='/lesson' element={ <ReviewList />}></Route>
+            <Route path='/review' element={ <ReviewDetail />}></Route>
+          </Routes>
+        <ToastContainer />
+        </div>
+      </QueryClientProvider>
     </>
   )
 }
