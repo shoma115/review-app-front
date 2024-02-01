2b69f874e76dfe1892501d2fffae9cf45b076b41
shoma115 <xiangzhendaogen8@gmail.com>
2024-02-02 08:30:59 +0900

    より固く

--- src/Functions/useMutationWithToast.js
+++ src/Functions/useMutationWithToast.js
@@ -1,18 +1,13 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 
-// mutationFnにはAPI、queryKeyName、successMessage、errorMessageには文字列を投げる
-export const useMutationWithToast = (mutationFn, queryKeyName, successMessage, errorMessage) => {
-  const queryClient = useQueryClient();
+// mutationFn、successFnにはメソッドを投げる。
+export const useMutationWithToast = (mutationFn, successFn, errorMessage) => {
 
   return useMutation
   ({
     mutationFn: mutationFn, 
-    onSuccess: () => {
-      // クエリを無効化。データの再フィッチを行い、キャッシュのデータを最新に保つ。
-      queryClient.invalidateQueries([queryKeyName]);
-      toast.success(successMessage);
-    },
+    onSuccess: successFn,
     onError: (error) => {
       const { data } = error.response ? error.response.data : null;
       if(data && data.errors) {
