941a8c3ae9955ed1e423aeaedd4debaa35b2f85e
shoma115 <xiangzhendaogen8@gmail.com>
2024-02-07 10:16:35 +0900

    検索APIとQueryの実装＋ちょっと投稿モーダル

--- src/APIs/ReviewAPI.js
+++ src/APIs/ReviewAPI.js
@@ -1,5 +1,6 @@
 import axiosApiSetBaseURL from '../BaseURL';
 
+// 授業のレビューの取得
 export const getAPI = async(lesson) => {
  try {
   const response = await axiosApiSetBaseURL.get(`api/review?lesson=${ lesson }`)
@@ -12,6 +13,28 @@ export const getAPI = async(lesson) => {
  }
 }
 
+// レビューの検索
+export const searchAPI = async(searchWord, lesson) => {
+  const searchParams = {
+    search: searchWord ? searchWord : null, 
+  }
+
+  const fliteredSearchParams = Object.fromEntries(
+    Object.entries(searchParams).filter(([key, value]) => value !== null)
+  );
+  const queryParams = new URLSearchParams(fliteredSearchParams).toString();
+  try {
+    const response = await axiosApiSetBaseURL.get(`api/review/search?lesson=${ lesson }&${ queryParams }`)
+    const data = response.data.data;
+    return data
+  }
+  catch(error) {
+    console.log(error)
+    throw(error);
+  }
+}
+
+// 授業レビューの投稿
 export const postAPI = async(title, ease, enrichment, content, userId, lessonId,) => {
   try {
     await axiosApiSetBaseURL.post("/api/review", {
--- src/ReviewDetail/reviewDetailTable.js
+++ src/ReviewDetail/reviewDetailTable.js
@@ -5,24 +5,32 @@ import FrequentWords from './FrequentWords.js';
 import SearchBox from '../commonComponents/SearchBox.js';
 import LessonDetail from './lessonDetail.js';
 import { useLocation } from 'react-router-dom';
-import searchReviewAPI from './searchReviewAPI.js';
 import { useState } from 'react';
 import Icon from '@mui/material/Icon';
-import { useGetReview } from '../Querys/ReviewQuery.js';
+import * as reviewQuery from '../Querys/ReviewQuery.js';
+import ReviewPost from '../ReviewPost/ReviewPostModal.js';
 
-function ReviewDetail() {
-  const location = useLocation().state.lesson;
-  // ↓この子がundefined投げてます。修正対象
-  const { data, isLoading, isError } = useGetReview(location.id);
-  console.log(data);
-  const [ review, setReview ] = useState()
+const ReviewDetail = ()  => {
+  // 投稿モーダルの開閉
+  const [ open, setOpen ] = useState(false);
+  const [onSearch, setOnSearch] = useState(false);
   const [inputValue, setInputValue] = useState(null);
+  const [searchWord, setSearchWord] = useState(null)
+  const location = useLocation().state.lesson;
+  // useQueryを使用しているメソッドは、引数が変化した場合は自動で再フィッチされるので注意。
+  const { data, isLoading, isError } = onSearch ? reviewQuery.useSearch(searchWord, location.id) : reviewQuery.useGet(location.id);
+  
   const handleInputChange = (event) => {
     setInputValue(event.target.value); 
   }
 
   const handleSearch = () => {
-    searchReviewAPI(inputValue, location.id, setReview);
+    setSearchWord(inputValue);
+    setOnSearch(true)
+  }
+
+  const handleOpne = () => {
+    setOpen(true)
   }
 
   if(isLoading) {
@@ -47,7 +55,8 @@ function ReviewDetail() {
           topics="テスト"
         />
       </FilterReview>
-      <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
+      <Icon sx={{ fontSize: 30 }} onClick={handleOpne}>add_circle</Icon>
+        <ReviewPost open={open} setOpen={setOpen} lessonId={location.id} />
       <Review reviews={ data.reviews } />
     </>
   )
